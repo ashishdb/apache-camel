74cc4210b51049ed19df55b6040075214cb28393
Ashish Kumar <ashish-j.kumar@db.com>
2021-02-12 15:34:16 +0530

    Apache Camel with SpringBoot Initial Changes

---
+++ SpringBootApacheCamel/src/test/java/com/db/eng/day/feb/SpringBootApacheCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.eng.day.feb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
